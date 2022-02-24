57d826d457ae227b9b4a6ff9cba36eeb5e46e240
AlicornIndustries <29941722+AlicornIndustries@users.noreply.github.com>
2022-02-23 19:40:10 -0600

    Alphabetized list of students in grade

--- typescript/exercism_grade-school.ts
+++ typescript/exercism_grade-school.ts
@@ -1,6 +1,9 @@
 /*
-Next steps: figure out how we want to do student IDs/data structure
-
+We could go deep on data structures for the class roster. This is quick-n'-dirty.
+Known problems:
+* Doesn't store the list sorted alphabetically, has to be explicitly sorted each time
+* Similarly, no proper indexing for e.g. "find students in grade 2" without searching whole roster
+* Hard to get from student to student ID number
 
 */
 
@@ -8,34 +11,59 @@ Next steps: figure out how we want to do student IDs/data structure
 class GradeSchool {
 
 
-    private classRoster: object = {}; // {  id:{name,age} } ?? (Exercise doesn't ask for ID)
-    private nextID: number = 0;
-    // We could use a UUID if we wanted to get funky
+    private _classRoster: StudentByID = {};
+    //private _classRoster = new Map<number,Student>(); // experimented with using a map
+    private _nextID: number = 0;
+    // We could use a UUID or AtomicInteger if we wanted to get funky
     // For now, just an incrementing int.
-
-
-
+    // In present state, we don't actually do much with student IDs
 
 
     roster() {
         // (I would call this either getRoster or use a proper getter)
-        throw new Error('Remove this statement and implement this function')
+        // Format: [1: ['name of student in grade 1','another student in grade 1], 2:[]]
+
+        console.log(Object.entries(this._classRoster))
+
+        // TODO
+
+
     }
 
     add(name: string, grade: number) {
-       // this.classRoster[nextID] = {'name': name, 'grade': grade}
+       this._classRoster[this._nextID] = {'name': name, 'grade': grade}
+       //this._classRoster.set(this._nextID,{'name':name,'grade':grade}) // the map way to do it
+       this._nextID++;
     }
 
     grade(num: number) {
         // Return alphabetized array of students in grade num
-
-
-        throw new Error('Remove this statement and implement this function')
+        // Naive solution: loop over _classRoster
+        // Looks like return format desired is just array of students' names
+        return Object.values(this._classRoster).
+            filter(student => student['grade'] === num).
+            map(student => student.name).
+            sort()
     }
 }
 
 interface Student {
-    id: number;
     name: string;
-    grade: string
+    grade: number
 }
+interface StudentByID {
+    [key: number]: Student
+}
+
+console.clear()
+const school = new GradeSchool();
+school.add('Alice',1)
+school.add('Bob',2)
+school.add('Bertha',2)
+school.add('Xerxes',3)
+school.roster()
+
+
+// const myMap = new Map();
+// myMap.set('100',{name: 'Alice', grade: 2})
+// console.log(myMap)

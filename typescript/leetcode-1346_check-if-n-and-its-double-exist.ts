e8753428223862e1be6832e6dae0a1c1c224d108
AlicornIndustries <29941722+AlicornIndustries@users.noreply.github.com>
2022-10-29 15:44:26 -0500

    Update leetcode-1346_check-if-n-and-its-double-exist.ts
    Another option using Set.

--- typescript/leetcode-1346_check-if-n-and-its-double-exist.ts
+++ typescript/leetcode-1346_check-if-n-and-its-double-exist.ts
@@ -1,24 +1,22 @@
+/*
+1346. Check If N and Its Double Exist
+*/
+
 function checkIfExist(arr: number[]): boolean {
-    let dic: { [key: number]: number  } = {};
+
+    let doublesAndHalves = new Set();
 
     for(let i=0; i<arr.length; i++) {
         // if double or half of current arr[i] exists in array, we've found it
-        if(dic.hasOwnProperty(arr[i]*2) || dic.hasOwnProperty(arr[i]/2)) {
+        //console.log(`i: ${i}, arr[i]: ${arr[i]}`)
+        //console.log(doublesAndHalves)
+        if(doublesAndHalves.has(arr[i])) {
             return true;
-        } 
+        }
         else {
-            dic[arr[i]] = i;
+            doublesAndHalves.add(arr[i]*2);
+            doublesAndHalves.add(arr[i]/2)
         }
     }
-
     return false;
 };
-
-/*
-Kinda like 2sum, store things along the way.
-Places to improve: not happy about the arr[i]/2 part, feels like there should be a more elegant approach.
-
-****
-This would be a good place to use Set.
-
-*/
